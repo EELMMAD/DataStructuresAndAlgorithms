2d4dd3bfa024a71a008094ca709069200cae9a59
EELMMAD <elmiramadadi@gmail.com>
2022-08-27 12:58:08 +0200

    Initial commit

---
+++ src/main/java/se/codewithMosh/elmira/theBigONotation/BigO.java
@@ -0,0 +1,9 @@
+package se.codewithMosh.elmira.theBigONotation;
+
+public class BigO {
+    //The Big O notation is a mathematical notation that describe the limiting
+    // behaviour of a function when the argument tends towards a particular
+    // value or infinity
+
+    //We use big O to describe the performance of an algorithm
+}
