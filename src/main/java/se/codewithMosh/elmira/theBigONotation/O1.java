528b974e8e3acaef4a175b6d3137dbc506a86013
EELMMAD <elmiramadadi@gmail.com>
2022-08-27 13:06:02 +0200

    O(1) runtime complexity

---
+++ src/main/java/se/codewithMosh/elmira/theBigONotation/O1.java
@@ -0,0 +1,8 @@
+package se.codewithMosh.elmira.theBigONotation;
+
+public class O1 {
+    public void log(int[] numbers){
+        //O(1)   runtime complexity
+        System.out.println(numbers[0]);
+    }
+}
