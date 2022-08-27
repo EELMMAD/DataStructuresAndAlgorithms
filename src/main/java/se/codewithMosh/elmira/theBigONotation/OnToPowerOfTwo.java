57e007ec5cdc9a2fbdfb2994df8fe44f799dd69f
EELMMAD <elmiramadadi@gmail.com>
2022-08-27 14:20:23 +0200

    O(n ^ 2) runtime complexity

---
+++ src/main/java/se/codewithMosh/elmira/theBigONotation/OnToPowerOfTwo.java
@@ -0,0 +1,26 @@
+package se.codewithMosh.elmira.theBigONotation;
+
+public class OnToPowerOfTwo { //O(n ^2)   O(n * n)
+    //nested loop
+    //printing all combinations of items in an array
+
+    public void log(int[] numbers){
+        //O(n ^ 2)  runtime complexity
+
+        for(int number : numbers)
+            System.out.println(number); //O(n)
+        for (int first : numbers) //O(n)
+            for(int second : numbers) //O(n)
+                System.out.println(first + ", " + second);
+    }
+
+    public void log1(int[] numbers){
+        //O(n ^ 3)  runtime complexity   (grow much more slow)
+
+        for (int first : numbers) //O(n)
+            for(int second : numbers) //O(n)
+                for (int third : numbers) { //O(n)
+                    System.out.println(first + ", " + second + ", " + third);
+                }
+    }
+}
