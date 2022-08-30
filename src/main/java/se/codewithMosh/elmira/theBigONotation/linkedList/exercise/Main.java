aec4007ef319a9563faa7a06995afb661c0f6ef9
EELMMAD <elmiramadadi@gmail.com>
2022-08-30 23:29:39 +0200

    run addLast method

---
+++ src/main/java/se/codewithMosh/elmira/theBigONotation/linkedList/exercise/Main.java
@@ -0,0 +1,10 @@
+package se.codewithMosh.elmira.theBigONotation.linkedList.exercise;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+    }
+}
