1e5d2cd7d2178ed83c7a4f464c10ee17854dd806
EELMMAD <elmiramadadi@gmail.com>
2022-08-30 21:58:13 +0200

    link list

---
+++ src/main/java/se/codewithMosh/elmira/theBigONotation/linkedList/Main.java
@@ -0,0 +1,22 @@
+package se.codewithMosh.elmira.theBigONotation.linkedList;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        System.out.println(list);
+        System.out.println(list.contains(10));
+        System.out.println(list.indexOf(10));
+        list.addFirst(5);
+        System.out.println(list);
+        list.removeLast();
+        System.out.println(list);
+        String array = list.toString();
+        System.out.println(array);
+        System.out.println(list.size());
+    }
+}
