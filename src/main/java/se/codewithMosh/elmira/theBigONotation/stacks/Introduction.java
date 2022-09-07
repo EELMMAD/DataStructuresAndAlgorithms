7bc310a9914788c146c0a8f9885766db6ff8c20d
EELMMAD <elmiramadadi@gmail.com>
2022-09-07 23:03:04 +0200

    introduction of stacks

---
+++ src/main/java/se/codewithMosh/elmira/theBigONotation/stacks/Introduction.java
@@ -0,0 +1,25 @@
+package se.codewithMosh.elmira.theBigONotation.stacks;
+
+public class Introduction {
+    /*
+                      STACKS
+            * implement the undo feature
+            * Build compilers(eg syntax checking)
+            * Evaluate expressions (eg 1 + 2 * 3)
+            *Build navigation (eg forward/ back)
+
+            Last In First Out (LIFO)
+     */
+
+
+    /*
+                         OPERATIONS
+      O(1)   *push(item)    -add item on top of stack
+      O(1)   *pop()         -removes item on the top
+      O(1)   *peak()        -return item on the top without removing it from the stack
+      O(1)   *isEmpty()     -is the stack is empty or not
+
+
+      we can quickly insert or remove an item at the end of a linked list
+     */
+}
