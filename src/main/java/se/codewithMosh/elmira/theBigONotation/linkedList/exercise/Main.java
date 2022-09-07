f183a5b38c202f057d3d7e1e4b4b3c95e95c5ce1
EELMMAD <elmiramadadi@gmail.com>
2022-09-07 22:44:05 +0200

    test methods

--- src/main/java/se/codewithMosh/elmira/theBigONotation/linkedList/exercise/Main.java
+++ src/main/java/se/codewithMosh/elmira/theBigONotation/linkedList/exercise/Main.java
@@ -18,16 +18,18 @@ public class Main {
         System.out.println("second size: " + list.size());
         list.addLast(20);
         list.addLast(30);
+        list.reverse();
         System.out.println("third size: " + list.size());
+
 
         var array = list.toArray();
         System.out.println("List: " + Arrays.toString(array));
-        
+
         var array2 = list.toArray();
         System.out.println("To array: " + Arrays.toString(array2));
 
-        list.removeLast();
-        list.removeFirst();
+        //list.removeLast();
+       // list.removeFirst();
         System.out.println("forth size: " + list.size());
 
 
@@ -44,7 +46,7 @@ public class Main {
         System.out.println(list.contains(10));
 
         //test removeFirst
-        list.removeFirst();
+       // list.removeFirst();
 
         //test removeLast
        // list.removeLast();
