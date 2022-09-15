77def84ec4a75f109bb38313c23f388d899cad42
EELMMAD <elmiramadadi@gmail.com>
2022-09-15 20:44:24 +0200

    add StringReverser to stack

---
+++ src/main/java/se/codewithMosh/elmira/theBigONotation/stacks/reversingTheStrinExercise/StringReverser.java
@@ -0,0 +1,25 @@
+package se.codewithMosh.elmira.theBigONotation.stacks.reversingTheStrinExercise;
+
+import java.util.Stack;
+
+public class StringReverser {
+    public String reverse(String input) {
+        if (input == null)
+            throw new IllegalArgumentException();
+        Stack<Character> stack = new Stack<>();
+
+        //for (int i = 0; i <input.length(); i++){
+        // stack.push(input.charAt(i));
+
+
+        //in java we can not use a forEach loop for a string, but
+        // we can convert that String to a Character Array and then use forEach loop for that array
+        for (char ch : input.toCharArray())
+            stack.push(ch);
+
+        StringBuffer reversed = new StringBuffer();
+        while (!stack.empty())
+            reversed.append(stack.pop());
+        return reversed.toString();
+    }
+}
