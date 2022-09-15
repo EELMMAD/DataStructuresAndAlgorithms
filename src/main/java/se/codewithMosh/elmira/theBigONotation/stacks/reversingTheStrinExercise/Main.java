22fae3ad7d84bf4c0526cdada2a7d2e30c0b98e1
EELMMAD <elmiramadadi@gmail.com>
2022-09-15 20:44:43 +0200

    test StringReverser

---
+++ src/main/java/se/codewithMosh/elmira/theBigONotation/stacks/reversingTheStrinExercise/Main.java
@@ -0,0 +1,11 @@
+package se.codewithMosh.elmira.theBigONotation.stacks.reversingTheStrinExercise;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "abcd";
+
+        StringReverser reverser = new StringReverser();
+        var result = reverser.reverse(null);
+        System.out.println(result);
+    }
+}
