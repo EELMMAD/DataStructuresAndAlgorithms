7e93c1a18ebf8dc7d0a012cc8cb9c56668662ff2
EELMMAD <elmiramadadi@gmail.com>
2022-09-18 00:02:35 +0200

    add more features to edge classes

--- src/main/java/se/codewithMosh/elmira/theBigONotation/stacks/balancedExpressions/Expression.java
+++ src/main/java/se/codewithMosh/elmira/theBigONotation/stacks/balancedExpressions/Expression.java
@@ -6,12 +6,20 @@ public class Expression {
 public boolean isBalanced(String input){
      Stack<Character> stack = new Stack<>();     //create a stack to store our left bracket
      for(char ch : input.toCharArray()){
-          if(ch == '(')
+          if(ch == '(' || ch == '<' || ch =='[' || ch == '{')
                stack.push(ch);
 
-          if(ch == ')') {
+          if(ch == ')' || ch == '>' || ch ==']' || ch == '}') {
                if(stack.empty()) return false;
-               stack.pop();
+              var top =  stack.pop();
+              if(
+                      (ch == ')' && top != '(') ||
+                              (ch == '>' && top != '<') ||
+                              (ch == ']' && top != '[') ||
+                              (ch == '}' && top != '{')
+
+              )
+              return false;
           }
      }
      return stack.empty();
