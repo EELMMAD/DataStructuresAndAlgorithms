c053e93c90ec00d4eae64eb86dbd807fdefd83c1
EELMMAD <elmiramadadi@gmail.com>
2022-11-21 20:58:03 +0100

    add reverse()

--- src/main/java/se/codewithMosh/elmira/queuse/Main.java
+++ src/main/java/se/codewithMosh/elmira/queuse/Main.java
@@ -1,9 +1,6 @@
 package se.codewithMosh.elmira.queuse;
 
-import java.util.ArrayDeque;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,8 +24,21 @@ public class Main {
         queue.add(10);
         queue.add(20);
         queue.add(30);
+        reverse(queue);
+        System.out.println(queue);
+
+        /*
         var front =  queue.remove();
         System.out.println(front);
         System.out.println(queue);
+         */
+    }
+
+    public static void reverse(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty())
+            stack.push(queue.remove());
+        while (!stack.isEmpty())
+            queue.add(stack.pop());
     }
 }
