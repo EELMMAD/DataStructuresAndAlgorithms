3aec9cec36256207a37616c47bd5b262ba881ce2
EELMMAD <elmiramadadi@gmail.com>
2022-11-21 11:47:10 +0100

    do refactoring

--- src/main/java/se/codewithMosh/elmira/theBigONotation/stacks/balancedExpressions/Expression.java
+++ src/main/java/se/codewithMosh/elmira/theBigONotation/stacks/balancedExpressions/Expression.java
@@ -1,27 +1,39 @@
 package se.codewithMosh.elmira.theBigONotation.stacks.balancedExpressions;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Expression {
-public boolean isBalanced(String input){
-     Stack<Character> stack = new Stack<>();     //create a stack to store our left bracket
-     for(char ch : input.toCharArray()){
-          if(ch == '(' || ch == '<' || ch =='[' || ch == '{')
-               stack.push(ch);
 
-          if(ch == ')' || ch == '>' || ch ==']' || ch == '}') {
-               if(stack.empty()) return false;
-              var top =  stack.pop();
-              if(
-                      (ch == ')' && top != '(') ||
-                              (ch == '>' && top != '<') ||
-                              (ch == ']' && top != '[') ||
-                              (ch == '}' && top != '{')
+    private final List<Character> leftBrackets = Arrays.asList('(', '<', '[', '{');
+    private final List<Character> rightBrackets = Arrays.asList(')', '>', ']', '}');
 
-              )
-              return false;
-          }
-     }
-     return stack.empty();
-}
+    public boolean isBalanced(String input) {
+        Stack<Character> stack = new Stack<>();     //create a stack to store our left bracket
+        for (char ch : input.toCharArray()) {
+            if (isLeftBracket(ch))
+                stack.push(ch);
+
+            if (isRightBracket(ch)) {
+                if (stack.empty()) return false;
+
+                var top = stack.pop();
+                if (bracketsMatch(top, ch)) return false;
+            }
+        }
+        return stack.empty();
+    }
+
+    private boolean isLeftBracket(char ch) {
+        return leftBrackets.contains(ch);
+    }
+
+    private boolean isRightBracket(char ch) {
+        return rightBrackets.contains(ch);
+    }
+
+    private boolean bracketsMatch(char left, char right) {
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
+    }
 }
