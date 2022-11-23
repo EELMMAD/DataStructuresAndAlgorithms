9f11b74c84a4491989733dc8fd1c04c4321885e1
EELMMAD <elmiramadadi@gmail.com>
2022-11-23 10:10:36 +0100

    QueueReverser.java

---
+++ src/main/java/se/codewithMosh/elmira/queuse/priorityQueues/exercises/QueueReverser.java
@@ -0,0 +1,36 @@
+package se.codewithMosh.elmira.queuse.priorityQueues.exercises;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+/*
+Given an integer K and a queue of integers, write code to reverse
+the order of the first K elements of the queue.
+Input: [10, 20, 30, 40, 50]  k = 3
+Output: [*10*, *20*, *30*, 40, 50]
+ */
+public class QueueReverser {
+    public static void reverse(Queue<Integer> queue, int k) {
+        if (k < 0 || k > queue.size())
+            throw new IllegalArgumentException();
+
+        Stack<Integer> stack = new Stack<>();
+
+        //Dequeue the first K elements from the queue
+        //and push them onto the stack
+        for(int i = 0; i < k; i++){
+            stack.push(queue.remove());
+        }
+
+        //Enqueue the content of the stack at the
+        //back of the queue
+        while (!stack.isEmpty())
+            queue.add(stack.pop());
+
+        //Add the remaining items at the queue(items after the first K elements) to
+        //the back of the queue and remove them from the beginning of the queue
+        for(int i = 0; i < queue.size() - k; i++){}
+         queue.add(queue.remove());
+    }
+}
