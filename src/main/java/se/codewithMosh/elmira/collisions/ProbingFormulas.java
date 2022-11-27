dd72a2ab1ecc0773be1e854c8b623081af805763
EELMMAD <elmiramadadi@gmail.com>
2022-11-27 18:37:11 +0100

    ProbingFormulas

---
+++ src/main/java/se/codewithMosh/elmira/collisions/ProbingFormulas.java
@@ -0,0 +1,11 @@
+package se.codewithMosh.elmira.collisions;
+
+public class ProbingFormulas {
+    /*
+    Linear:       (hash1 + i) % table_size
+
+    Quadratic:    (hash1 + i ^2) % table_size
+
+    Double Hash:  (hash1 + i * hash2) % table_size
+     */
+}
